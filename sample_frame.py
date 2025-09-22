a5a6f46fc84027aea32ce85f1629ffdf54e6e117
Anthony DeMatteo <anthde13@gmail.com>
2025-09-21 21:29:10 -0400

    docs - sample_frame TODO

--- sample_frame.py
+++ sample_frame.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-
+#TODO: sample first few seconds for title cards
 def sample_frames(video_path, output_dir, interval_seconds=5):
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
